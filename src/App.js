d2b203df6f81996123e629bc41e0e557ac7dae28
OneMoarDevs <59660661+OneMoarDevs@users.noreply.github.com>
2023-05-25 22:21:00 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TodoForm from './features/todos/TodoForm';
+import TodoList from './features/todos/TodoList';
+
+function App() {
+  return (
+    <div className='App'>
+      <TodoForm />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const PRIORITY_OPTIONS = ['low', 'medium', 'high'];
+export const CATEGORY_OPTIONS = ['chores', 'work', 'school', 'personal', 'others'];
